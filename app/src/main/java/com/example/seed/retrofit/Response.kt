ef2abcf247c8e503fbef592ba4e5e4ad5a36807e
FollowTech <927394019@qq.com>
2022-05-24 14:00:19 +0800

    wrote a readme.txt

---
+++ app/src/main/java/com/example/seed/retrofit/Response.kt
@@ -0,0 +1,27 @@
+package com.example.seed.retrofit
+
+import com.google.gson.annotations.SerializedName
+
+data class Response(
+
+	@field:SerializedName("Response")
+	val response: List<ResponseItem?>? = null
+)
+
+data class ResponseItem(
+
+	@field:SerializedName("image")
+	val image: String? = null,
+
+	@field:SerializedName("pk")
+	val pk: Int? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("body")
+	val body: String? = null,
+
+	@field:SerializedName("category")
+	val category: String? = null
+)
